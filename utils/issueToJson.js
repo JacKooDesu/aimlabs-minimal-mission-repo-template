5db5a7c619474ccad5ac2d06cf3a1c270602a64a
JackooDesu <45838705+JacKooDesu@users.noreply.github.com>
2025-06-14 15:16:07 +0800

    add read mission info

--- utils/issueToJson.js
+++ utils/issueToJson.js
@@ -18,39 +18,56 @@
 //
 // - [x] Agree to the terms
 
-var fs = require("fs");
+import fs from "fs/promises";
 
 const user = process.argv[2];
 const issueMdArr = process.argv[3].split("\n");
 
-(function convertIssueToJson() {
+(async function convertIssueToJson() {
   let missionName = issueMdArr[2];
   let missionVersion = issueMdArr[6];
   let missionGitUrl = issueMdArr[10];
   let missionArchiveUrl = issueMdArr[14];
 
+  let missionInfoUrl = new URL(missionGitUrl);
+  missionInfoUrl.hostname = "api.github.com";
+  let pathName = `repos${missionInfoUrl.pathname}/contents`;
+  if (missionInfoUrl.searchParams.get("path")) {
+    pathName += `/${missionInfoUrl.searchParams.get("path")}`;
+  }
+  pathName += "/mission.json";
+  missionInfoUrl.pathname = pathName;
+
+  let info = await fetch(missionInfoUrl)
+    .then((res) => res.json())
+    .then((data) => JSON.parse(atob(data.content)))
+    .catch((err) => {
+      throw err;
+    });
+
   missionName.replace(/ /g, "-");
 
-  fs.mkdir(`./contents/${user}/${missionName}`, { recursive: true }, (err) => {
-    if (err) throw err;
+  await fs
+    .mkdir(`./contents/${user}/${missionName}`, { recursive: true })
+    .then(() => {
+      let missionJson = {
+        name: missionName,
+        author: user,
+        version: missionVersion,
+        gitUrl: missionGitUrl,
+        archiveUrl: missionArchiveUrl,
+        info: info,
+      };
 
-    missionJson = {
-      name: missionName,
-      author: user,
-      version: missionVersion,
-      gitUrl: missionGitUrl,
-      archiveUrl: missionArchiveUrl,
-    };
-    fs.writeFile(
-      `./contents/${user}/${missionName}/${missionVersion}.json`,
-      JSON.stringify(missionJson, null, 2),
-      (err) => {
-        if (err) throw err;
-      }
-    );
-  });
+      return fs.writeFile(
+        `./contents/${user}/${missionName}/${missionVersion}.json`,
+        JSON.stringify(missionJson, null, 2)
+      );
+    })
+    .catch((err) => {
+      console.error("Error:", err);
+    });
 
-  // create metadata file for PR create
   // create metadata file for PR create
   fs.writeFile(
     "result-path",
